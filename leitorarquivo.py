62eaab09cd7588f225685635c23e9b0d1f575996
k1000lep <camile.fpedrolo@gmail.com>
2022-08-25 09:16:20 -0300

    novo arquivo e alteração no leitor

--- leitorarquivo.py
+++ leitorarquivo.py
@@ -8,8 +8,3 @@ class LeitorArquivo:
         return self.valores
 
 
-if __name__ == '__main__':
-    leitor = LeitorArquivo("data.txt")
-    listaValores = leitor.getValores()
-    print(listaValores)
-
