a4c0ff047538340854b6426f4ba9c33fee144a1d
k1000lep <camile.fpedrolo@gmail.com>
2022-08-25 09:33:18 -0300

    criei grafico

--- geragrafico.py
+++ geragrafico.py
@@ -1,8 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from leitorarquivo import LeitorArquivo
  
 def main():
     leitor = LeitorArquivo('data.txt')
     valores = leitor.getValores()
     print(valores)
- 
+    plt.plot(valores)
+    plt.show()
+
 main()
+
+
+
